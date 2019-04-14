2b3b0132f8185afb5347d13d1b36db152774db02
WestD3 <47015230+WestD3@users.noreply.github.com>
2019-04-14 15:47:55 -0500

    Revert "Update mpg.py"
    This reverts commit 63d0b28ed4c5eaa7d92b012bb783c5b4ed25f017.

--- mpg.py
+++ mpg.py
@@ -4,34 +4,21 @@
 print("The Miles Per Gallon application")
 print()
 
-choice = "y"
+# get input from the user
+miles_driven = float(input("Enter miles driven:         "))
+gallons_used = float(input("Enter gallons of gas used:  "))
 
-while choice == "y":
-    # get input from the user
-    miles_driven = float(input("Enter miles driven:         "))
-    gallons_used = float(input("Enter gallons of gas used:  "))
-    cost_per_gallon = float(input("Enter cost per gallon:   "))
-
-    if miles_driven <= 0:
-        print("Miles driven must be greater than zero. Please try again.")
-    elif gallons_used <= 0:
-        print("Gallons used must be greater than zero. Please try again.")
-    else:
-        # calculate and display miles per gallon
-        mpg = round((miles_driven / gallons_used), 2)
-        gas_cost = round((gallons_used * cost_per_gallon), 2)
-        cost_per_mile = round((gas_cost / miles_driven), 1)
-   
-
-    print()
+if miles_driven <= 0:
+    print("Miles driven must be greater than zero. Please try again.")
+elif gallons_used <= 0:
+    print("Gallons used must be greater than zero. Please try again.")
+else:
+    # calculate and display miles per gallon
+    mpg = round((miles_driven / gallons_used), 2)
     print("Miles Per Gallon:          ", mpg)
-    print("Total Gas Cost:            ", gas_cost)
-    print("Cost Per Mile:             ", cost_per_mile)
 
-    print()
-    yes_or_no = (input("Get entries for another trip (y/n)?"))
+print()
+print("Bye")
 
-    if yes_or_no=='n':
-        break
 
 
