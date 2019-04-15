2e383c0435439c896e9390558e408e669ec8b085
WestD3 <47015230+WestD3@users.noreply.github.com>
2019-04-14 22:19:47 -0500

    Revert "Update future_value.py"
    This reverts commit 16d1daa5a7ad86e7653ae8ab5d616076e7d4cf78.

--- future_value.py
+++ future_value.py
@@ -1,50 +1,26 @@
 #!/usr/bin/env python3
-def get_number(prompt, low, high):
-    while True:
-        number = float(input(prompt))
-        if number > low and number <= high:
-            is_valid = True
-            return number
-        else:
-            print("Entry must be greater than", low,
-                  "and less than or equal to", high,
-                  "Please try again.")  
-
-def get_integer(prompt, low, high):
-    while True:
-        number = int(input(prompt))
-        if number > low and number <= high:
-            is_valid = True
-            return number
-        else:
-            print("Entry must be greater than", low, 
-                  "and less than or equal to", high,
-                  "Please try agian.")     
-
+        
 def calculate_future_value(monthly_investment, yearly_interest, years):
     # convert yearly values to monthly values
     monthly_interest_rate = yearly_interest / 12 / 100
     months = years * 12
 
     # calculate future value
-    print("Enter calculate_future_value()")
     future_value = 0.0
     for i in range(0, months):
         future_value += monthly_investment
         monthly_interest = future_value * monthly_interest_rate
         future_value += monthly_interest
 
-        future_value = round(future_value, 2)
-        print("i = ", i, "future value = ", future_value)
     return future_value
 
 def main():
     choice = "y"
     while choice.lower() == "y":
         # get input from the user
-        monthly_investment =  get_number("Enter monthly investment:\t", 0, 10000)
-        yearly_interest_rate = get_number("Enter yearly interest rate:\t", 0, 15)
-        years = get_integer("Enter number of years:\t\t", 0, 50)
+        monthly_investment = float(input("Enter monthly investment:\t"))
+        yearly_interest_rate = float(input("Enter yearly interest rate:\t"))
+        years = int(input("Enter number of years:\t\t"))
 
         # get and display future value
         future_value = calculate_future_value(
